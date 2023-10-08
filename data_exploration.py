fde5b678f97f3c3802d1272a29add24323bbf52c
ManuelMares <fmms@outlook.com>
2023-10-08 14:29:56 -0600

    added data sets

---
+++ data_exploration.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f84880944d962e69193edd6a986c3cb1cd29974be73969660f11f6268547ecfc
+size 2262
--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-"""
-    To properly work in this project, don't forget to read the readme
-"""
-#%%
-print("This is and example of an interactive cell in VSCode")
-
-
-# %%
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef097cb4452dc538c3efd797625fb1ffe5a9e888c7f7ade8c932d7edefc09ef6
+size 1376
