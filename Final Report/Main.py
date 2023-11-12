445676b831ea3014e08a5eafde90ebc8ce8b72d0
ManuelMares <fmms@outlook.com>
2023-11-12 14:31:21 -0700

    main

---
+++ Final Report/Main.py
@@ -0,0 +1,22 @@
+#%%
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sb
+
+
+
+
+df_indigenous_2015          = pd.read_csv("./NewDataSets/SourceDataSets/indigenous_2015.csv")
+df_indigenous_2020             = pd.read_csv("./NewDataSets/SourceDataSets/indigenous_2020.csv")
+df_literacy_2015            = pd.read_csv("./NewDataSets/SourceDataSets/literacy_2015.csv")
+df_literacy_2020            = pd.read_csv("./NewDataSets/SourceDataSets/literacy_2020.csv")
+df_population_2015          = pd.read_csv("./NewDataSets/SourceDataSets/population_2015.csv")
+df_population_2020          = pd.read_csv("./NewDataSets/SourceDataSets/population_2020.csv")
+df_poverty_2015             = pd.read_csv("./NewDataSets/SourceDataSets/poverty_2015.csv")
+df_poverty_2020             = pd.read_csv("./NewDataSets/SourceDataSets/poverty_2020.csv")
+df_work_situation_2015      = pd.read_csv("./NewDataSets/SourceDataSets/work_situation_2015.csv")
+df_work_situation_2020      = pd.read_csv("./NewDataSets/SourceDataSets/work_situation_2020.csv")
+
+# %%
+
+
